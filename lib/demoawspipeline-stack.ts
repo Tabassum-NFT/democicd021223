f91d662dc4e71239cfd2ee73fa5670d4d1d42654
Tabassum-NFT <tabassum@nexusft.tech>
2025-08-08 00:37:59 -0700

    first commit

---
+++ lib/demoawspipeline-stack.ts
@@ -0,0 +1,29 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import {CodePipeline, CodePipelineSource, ShellStep} from 'aws-cdk-lib/pipelines';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class DemoawspipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    //AWS CI_CD Pipeline
+
+    const democicdpipeline = new CodePipeline(this, 'demopipeline', {
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('', 'main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ],
+      }),
+    });
+
+  }
+    // example resource
+    // const queue = new sqs.Queue(this, 'DemoawspipelineQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+}
